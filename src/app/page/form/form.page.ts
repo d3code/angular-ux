5cdb3a94b99f09ad9b4a90a0052357d3d07c922a
Luke Sands <luke.sands@d3code.com.au>
2024-09-01 14:09:08 +1000

    update

--- src/app/page/form/form.page.ts
+++ src/app/page/form/form.page.ts
@@ -1,21 +1,67 @@
 import { Component } from '@angular/core';
 import { RouterLink } from '@angular/router';
+import { NgSelectModule } from '../../../../projects/ngux/src/lib/component/select/ng-select.module';
+import {
+  NgbDatepickerModule,
+  NgbDateStruct,
+} from '../../../../projects/ngux/src/lib/component/datepicker/datepicker.module';
+import { IconModule } from '../../../../projects/ngux/src/lib/component/icon/icon.module';
+import { FormsModule } from '@angular/forms';
 
 @Component({
   selector: 'app-form',
   standalone: true,
-  imports: [RouterLink],
+  imports: [
+    RouterLink,
+    FormsModule,
+    NgSelectModule,
+    NgbDatepickerModule,
+    IconModule,
+  ],
   template: `
     <h1>Form</h1>
 
-    <p>Forms are used to collect user input. This page demonstrates the various form elements available in the UX library.</p>
+    <p>
+      Forms are used to collect user input. This page demonstrates the various
+      form elements available in the UX library.
+    </p>
 
-    <h2>Button</h2>
-    <p>Buttons are used to submit forms, navigate to other pages, or trigger actions.</p>
-    <a routerLink="/form/button">Button</a>
+    <form class="my-8">
+      <div class="row">
+        <div class="input-group">
+          <input
+            class="form-control"
+            placeholder="yyyy-mm-dd"
+            name="dp"
+            [(ngModel)]="model"
+            ngbDatepicker
+            #d="ngbDatepicker"
+          />
+          <button (click)="d.toggle()">
+            <ux-icon name="calendar_month"></ux-icon>
+          </button>
+        </div>
+      </div>
+
+      <div class="row">
+        <ng-select
+          initialFocus
+          [items]="items"
+          bindLabel="title"
+          bindValue="id"
+          [searchable]="true"
+          placeholder="Search"
+        />
+      </div>
+    </form>
   `,
-  styles: ``
+  styles: ``,
 })
 export class FormPage {
+  model: NgbDateStruct;
 
+  items = [
+    { id: 1, title: 'item1' },
+    { id: 2, title: 'item2' },
+  ];
 }
